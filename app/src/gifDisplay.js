d5f154691cd16b1aecdacd92f7a2fd3118cd532e
James Sutherland <jrsutherland78@googlemail.com>
2019-05-21 23:42:35 +0100

    add #_getGifs

--- app/src/gifDisplay.js
+++ app/src/gifDisplay.js
@@ -14,8 +14,24 @@ class GifDisplay {
   _listeners() {
     let that = this;
     $("form").on('submit', function (e) {
+      that._getGifs();
       e.preventDefault();
     });
+  }
+
+  _getGifs(e) {
+    let searchItem = document.getElementById('searchItem').value
+    console.log(searchItem)
+    let gifUrl = this.url + "api_key=<%= iparam.api_key.key %>&q=" + searchItem + "&limit=9&offset=0&rating=G&lang=en"
+    this.client.request.get(gifUrl)
+      .then(
+        function(data) {
+          console.log(data)
+        },
+        function(error) {
+
+       }
+     )
   }
 
 };
