eeb477a350ec0d0bc0984a11ab254895d7dea6ec
James Sutherland <jrsutherland78@googlemail.com>
2019-05-22 12:26:23 +0100

    displays first 9 gifs based on search

--- app/src/gifDisplay.js
+++ app/src/gifDisplay.js
@@ -14,24 +14,40 @@ class GifDisplay {
   _listeners() {
     let that = this;
     $("form").on('submit', function (e) {
+      that._clearGifs();
       that._getGifs();
       e.preventDefault();
     });
   }
 
   _getGifs(e) {
+    let that = this
     let searchItem = document.getElementById('searchItem').value
     console.log(searchItem)
     let gifUrl = this.url + "api_key=<%= iparam.api_key.key %>&q=" + searchItem + "&limit=9&offset=0&rating=G&lang=en"
     this.client.request.get(gifUrl)
       .then(
-        function(data) {
-          console.log(data)
+        function(gifsData) {
+          that.gifResults = JSON.parse(gifsData.response)
+          that._displayGifs()
         },
         function(error) {
-
-       }
+        }
      )
+  }
+
+  _displayGifs() {
+    console.log(this.gifResults.data)
+    this.gifResults.data.forEach(function(element) {
+      let elem = document.createElement("img")
+      elem.src = element.images.fixed_width.url
+      document.getElementById("gifs").appendChild(elem)
+    })
+
+  }
+
+  _clearGifs() {
+    document.getElementById("gifs").innerHTML = ""
   }
 
 };
